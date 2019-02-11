13e53aa0fab2832bd780dc79a2dc95c0ef937865
agray5 <grayaprild@gmail.com>
2019-02-11 16:10:39 -0600

    Updates

---
+++ precache-manifest.c0651b2a9dae387528de77aaadc02716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b203cf54ec4e74f9e1b",
+    "url": "/Text-Adventure/static/js/runtime~main.6b203cf5.js"
+  },
+  {
+    "revision": "2a7e351d6f6b2841b780",
+    "url": "/Text-Adventure/static/js/main.2a7e351d.chunk.js"
+  },
+  {
+    "revision": "bceb70fd28210ecadc54",
+    "url": "/Text-Adventure/static/js/1.bceb70fd.chunk.js"
+  },
+  {
+    "revision": "2a7e351d6f6b2841b780",
+    "url": "/Text-Adventure/static/css/main.08d72841.chunk.css"
+  },
+  {
+    "revision": "5e6dadfcfe38d2f2ee5164c955140ae0",
+    "url": "/Text-Adventure/index.html"
+  }
+];
