f73058b2baf7dadea273716d1d9ab5811dbfbefd
agray5 <grayaprild@gmail.com>
2019-02-11 16:56:07 -0600

    Updates

--- precache-manifest.38a27de738c03ef610154e39ff97d4f9.js
+++ precache-manifest.38a27de738c03ef610154e39ff97d4f9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Text-Adventure/static/js/runtime~main.6b203cf5.js"
   },
   {
-    "revision": "5c9408db9fcea825834d",
-    "url": "/Text-Adventure/static/js/main.5c9408db.chunk.js"
+    "revision": "a23608ff2f2c71ef3e87",
+    "url": "/Text-Adventure/static/js/main.a23608ff.chunk.js"
   },
   {
     "revision": "bceb70fd28210ecadc54",
     "url": "/Text-Adventure/static/js/1.bceb70fd.chunk.js"
   },
   {
-    "revision": "5c9408db9fcea825834d",
+    "revision": "a23608ff2f2c71ef3e87",
     "url": "/Text-Adventure/static/css/main.08d72841.chunk.css"
   },
   {
-    "revision": "b4abe0862d1111258d423c809bf96a10",
+    "revision": "995db6c59e9bab503690a76d2d5755c1",
     "url": "/Text-Adventure/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Text-Adventure/precache-manifest.8c6d6342f193ad0bb155842dd0564fa0.js"
+  "/Text-Adventure/precache-manifest.38a27de738c03ef610154e39ff97d4f9.js"
 );
 
 workbox.clientsClaim();
